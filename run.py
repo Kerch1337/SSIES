c2ba799bfb8e725beac6a38b1126e990a700fa9d
Kornenko Kirill <kornenko_k@mail.ru>
2026-05-15 08:18:18 +0300

    Удаление лишнего

--- run.py
+++ run.py
@@ -1,15 +1,11 @@
-from ssies import hello
 from ssies import DM
 from pathlib import Path
-
-print(hello())
 
 parser = DM("f09dm92mar13.dat.gz")
 data = parser.parse_file()
 ds = parser.to_xarray(data)
 
-#parser.export_netcdf(ds, "out.nc")
-#parser.export_cdf(ds, "out.cdf")
+parser.export_netcdf(ds, "out.nc")
 parser.export_csv(ds, "out.csv")
 
 #print(ds)
--- src/ssies/__init__.py
+++ src/ssies/__init__.py
@@ -1,2 +1,1 @@
-from .core import hello
 from .dm import DM
